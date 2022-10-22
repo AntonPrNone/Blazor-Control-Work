c49fa9cf452d05c5086f16f90d4aeb8e891cdb75
Anton <yznikfortyni@gmail.com>
2022-10-22 14:27:25 +0300

    Добавьте файлы проекта.

---
+++ Blazor Control Work/Data/User.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Blazor_Control_Work.Data
+{
+    [BsonIgnoreExtraElements]
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Blazor Control Work/MongoExamples.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using Blazor_Control_Work.Data;
+
+namespace Blazor_Control_Work
+{
+    public class MongoExamples
+    {
+        public static User Find(string name) // Returns a document
+        {
+            var client = new MongoClient();
+            var database = client.GetDatabase("DB");
+            var collection = database.GetCollection<User>("Users");
+            var document = collection.Find(x => x.Name == name).FirstOrDefault();
+            return document;
+        }
+
+        public static void SaveValues(string name, User unit) // Replaces the document
+        {
+            var client = new MongoClient();
+            var database = client.GetDatabase("DB");
+            var collection = database.GetCollection<User>("Users");
+            collection.ReplaceOne(x => x.Name == name, unit);
+        }
+
+        public static void AddToDB(User user)
+        {
+            var client = new MongoClient();
+            var database = client.GetDatabase("DB");
+            var collection = database.GetCollection<User>("Users");
+            collection.InsertOne(user);
+
+        }
+    }
+}
